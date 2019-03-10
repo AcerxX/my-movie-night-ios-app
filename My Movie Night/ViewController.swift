66a3b6a0113ac8ccf3217b2ed1c7bc11a1aba6e6
Alexandru Mihai <alexandru.mihai@it-genetics.ro>
2019-03-10 12:07:04 +0200

    Initial Commit

---
+++ My Movie Night/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Movie Night
+//
+//  Created by Alexandru Mihai on 10/03/2019.
+//  Copyright © 2019 Alexandru Mihai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ My Movie NightTests/My_Movie_NightTests.swift
@@ -0,0 +1,34 @@
+//
+//  My_Movie_NightTests.swift
+//  My Movie NightTests
+//
+//  Created by Alexandru Mihai on 10/03/2019.
+//  Copyright © 2019 Alexandru Mihai. All rights reserved.
+//
+
+import XCTest
+@testable import My_Movie_Night
+
+class My_Movie_NightTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
