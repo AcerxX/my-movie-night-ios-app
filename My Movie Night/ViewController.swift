852afad6155374ec7c6d49c44db5aed6f25d2087
Alexandru Mihai <alexandru.mihai@it-genetics.ro>
2019-03-24 12:19:18 +0200

    add things

--- My Movie Night/ViewController.swift
+++ My Movie Night/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import WebKit
+import UserNotifications
 
 class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
     var webView: WKWebView!
@@ -17,14 +18,42 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
         webView.navigationDelegate = self
         webView.uiDelegate = self
         view = webView
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: .badge) { (granted, error) in
+            if error != nil {
+                // success!
+            }
+        }
+        
+        UIApplication.shared.applicationIconBadgeNumber = 0
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let url = URL(string: "http://mmnight.tk")!
+        let url = URL(string: "http://80.211.24.165")!
         webView.load(URLRequest(url: url))
-        webView.allowsBackForwardNavigationGestures = false
+        webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    func sendNotification() {
+        let content = UNMutableNotificationContent()
+        
+        //adding title, subtitle, body and badge
+        content.title = "Hey this is Simplified iOS"
+        content.subtitle = "iOS Development is fun"
+        content.body = "We are learning about iOS Local Notification"
+        content.badge = 1
+        
+        //getting the notification trigger
+        //it will be called after 1 second
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 60, repeats: true)
+        
+        //getting the notification request
+        let request = UNNotificationRequest(identifier: "SimplifiedIOSNotification", content: content, trigger: trigger)
+        
+        //adding the notification to notification center
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
     }
     
     func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
